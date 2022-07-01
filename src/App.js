d165711a3015d8025541bdce32e0477277e5a09e
meghamdas <meghamohandas19@gmail.com>
2022-07-01 12:43:41 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import './App.css'
+import Form from './components/Forms';
+
+function App(){
+
+    return (
+      <div>
+        <Form/>
+      </div>
+    ); 
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
